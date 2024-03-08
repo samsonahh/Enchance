259667e0fc492aab82049510dc0b394787235cec
Samson Wu <samsonwu05@gmail.com>
2024-03-08 17:17:59 -0500

    Revert "astar p1"
    This reverts commit d722440306a4cea3190aba245c19a41e67c5a466.

--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
@@ -10,7 +9,7 @@ public class GridManager : MonoBehaviour
     [SerializeField] private int _width, _height;
     [SerializeField] private Tile _tilePrefab;
 
-    [HideInInspector] public Vector3 MousePosition { get; private set; }
+    [HideInInspector] public Vector3 MousePosition;
 
     private Dictionary<Vector2, Tile> _tiles;
 
@@ -66,59 +65,5 @@ public class GridManager : MonoBehaviour
         }
 
         return null;
-    }
-    public Tile GetTileAtPosition(int x, int y)
-    {
-        if (_tiles.TryGetValue(new Vector2(x, y), out var tile))
-        {
-            return tile;
-        }
-
-        return null;
-    }
-
-    public List<Tile> AStarPathFind(int startX, int startY, int targetX, int targetY)
-    {
-        List<Tile> toSearch = new List<Tile>() { GetTileAtPosition(startX, startY) };
-        List<Tile> processed = new List<Tile>();
-
-        while (toSearch.Any())
-        {
-            Tile current = toSearch[0];
-            foreach(Tile t in toSearch)
-            {
-                if(t.F < current.F || t.F == current.F)
-            }
-
-            processed.Add(current);
-            toSearch.Remove(current);
-
-    
-        }
-
-        return new List<Tile>();
-    }
-
-    private List<Tile> FindNeighbors(Tile tile)
-    {
-        List<Tile> neighbors = new List<Tile>();
-
-        Tile right = GetTileAtPosition(tile.X + 1, tile.Y);
-        Tile left = GetTileAtPosition(tile.X - 1, tile.Y);
-        Tile up = GetTileAtPosition(tile.X, tile.Y + 1);
-        Tile down = GetTileAtPosition(tile.X, tile.Y - 1);
-
-        if (right != null) neighbors.Add(right);
-        if (left != null) neighbors.Add(left);
-        if (up != null) neighbors.Add(up);
-        if (down != null) neighbors.Add(down);
-
-        return neighbors;
-    }
-
-    private void CalculateAStarParameters(Tile t, int startX, int startY, int targetX, int targetY)
-    {
-        t.G = Vector2.Distance(new Vector2(t.X, t.Y), new Vector2(startX, startY));
-        t.H = Vector2.Distance(new Vector2(t.X, t.Y), new Vector2(targetX, targetY));
     }
 }
--- Assets/Scripts/Grid/Tile.cs
+++ Assets/Scripts/Grid/Tile.cs
@@ -10,19 +10,9 @@ public class Tile : MonoBehaviour
     [SerializeField] private GameObject _highlight;
     [SerializeField] private Collider _collider;
 
-    [HideInInspector] public int X {get; private set;}
-    [HideInInspector] public int Y {get; private set;}
-    [HideInInspector] public float G;
-    [HideInInspector] public float H;
-    [HideInInspector] public float F => G + H;
-
     public void Init(int x, int y)
     {
         _gridManager = GridManager.Instance;
-        
-        X = x;
-        Y = y;
-        
         bool isOffset = (x + y) % 2 == 1;
         _renderer.color = isOffset ? _offsetColor : _baseColor;
     }
