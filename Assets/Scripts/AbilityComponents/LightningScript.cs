2c8489a2c677e5b8992d71bc2c8da8f82e43f74a
Samson Wu <swu30@stuy.edu>
2024-03-15 15:31:37 -0400

    fixed lightning disappearing immediately upon boss contact

--- Assets/Scripts/AbilityComponents/LightningScript.cs
+++ Assets/Scripts/AbilityComponents/LightningScript.cs
@@ -7,14 +7,12 @@ public class LightningScript : MonoBehaviour
     [SerializeField] private GameObject _explosionPrefab;
     [SerializeField] private int _damage = 5;
     private float _strikeRadius;
-    private BossAI _boss;
     private SphereCollider _collider;
 
     private void Start()
     {
         transform.position = PlayerController.Instance.LastCircleWorldPosition;
         _strikeRadius = AbilityCaster.Instance.CurrentAbilities[AbilityCaster.Instance.SelectedAbility].CircleCastRadius;
-        _boss = GameObject.Find("Boss").GetComponent<BossAI>();
         _collider = GetComponent<SphereCollider>();
 
         _collider.radius = _strikeRadius;
@@ -22,20 +20,18 @@ public class LightningScript : MonoBehaviour
         Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
         CameraShake.Instance.Shake(0.1f, 0.1f);
 
-/*        if (Vector3.Distance(_boss.transform.position, transform.position) < _strikeRadius)
+        Collider[] collisions = Physics.OverlapSphere(transform.position, _strikeRadius);
+        if (collisions != null)
         {
-            _boss.TakeDamage(_damage);
-        }*/
-
-        Destroy(gameObject, 0.2f);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.TryGetComponent(out BossAI boss))
-        {
-            boss.TakeDamage(_damage);
-            Destroy(gameObject);
+            foreach(Collider collider in collisions)
+            {
+                if(collider.TryGetComponent(out BossAI bossAI))
+                {
+                    bossAI.TakeDamage(_damage);
+                }
+            }
         }
+
+        Destroy(gameObject, 0.3f);
     }
 }
